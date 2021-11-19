12bba280442e4f6bf7e77d9e2162be8bc7c62e22
Paul Smith <paul@waternumbers.co.uk>
2021-11-19 17:54:52 +0000

    undoing bad things...

---
+++ R/scaleImage.R
@@ -0,0 +1,75 @@
+#' Function for Generating a legend from fcol
+#'
+#' @param fcol
+#' @param lv
+#' @param nb
+#' @param w
+#' @param h
+#'
+#' @export
+scaleImage <- function(fcol,lv,nb=0,w=5,h=10){
+
+
+    lv <- sort(lv)
+    if(length(lv)<2){ stop("lv must have a length of at least 2") }
+
+    ## work out names and colours
+    lbl <- paste(lv)
+    pcol <- max(lv[1],-1e306)
+    plbl <- c("",lbl[1])
+    for(ii in 2:length(lv)){
+        pcol <- c(pcol, seq(utils::tail(pcol,1),min(lv[ii],1e306),length=nb+2)[-1])
+        plbl <- c(plbl,rep("",nb),lbl[ii])
+    }
+    pcol <- fcol(pcol)
+
+    
+    ## if first colour is transparent or NA
+    flg <- TRUE
+    while(flg){
+        if(is.na(pcol[1]) | substr(pcol[1],8,9) == "00"){
+            pcol <- pcol[-1]
+            plbl <- plbl[-1]
+        }else{
+            flg <- FALSE
+        }
+        if(length(pcol)==0){ stop("No valid colours to plot") }
+    }
+
+    
+    ## last highest value if color is NA or transparent
+    flg <- TRUE
+    while(flg){
+        if(is.na(pcol[length(pcol)]) | substr(pcol[length(pcol)],8,9) == "00"){
+            pcol <- utils::tail(pcol,-1)
+            plbl <- utils::tail(plbl,-1)
+        }else{
+            flg <- FALSE
+        }
+        if(length(pcol)==0){ stop("No valid colours to plot") }
+    }
+
+    ## remove Inf or -Inf from label
+    plbl <- gsub("[-Inf|Inf]","",plbl)
+
+    nrct <- length(pcol) ## number of rectangles
+
+    ## limits of the rectangles
+    xl <- 1; xr <- 2 ## left and right limits of rectangles
+    yb <- 1; yt <- 2 ## bottom and top limits of the rectanges
+
+    ## start plot
+    svglite::stringSVG({
+        graphics::par(mar=c(0.05,0.05,0.05,5))
+        plot(NA,type="n",ann=FALSE,xlim=c(xl,xr),ylim=c(yb,yt),
+             xaxt="n",yaxt="n",bty="n")
+        graphics::rect(
+                      xl,
+                      utils::head(seq(yb,yt,(yt-yb)/nrct),-1),
+                      xr,
+                      utils::tail(seq(yb,yt,(yt-yb)/nrct),-1),
+                      col=pcol
+                  )
+        graphics::mtext(plbl,side=4,at=utils::head(seq(yb,yt,(yt-yb)/nrct),-1),las=2,cex=1.7)
+    },width=w,height=h)
+}
---
+++ R/serveTiles.R
@@ -0,0 +1,203 @@
+#' Start a serving serving tiles from a raster data set with time attribute
+#' 
+#' @param dataFile file containing spatial data to be read by \code{terra::rast}
+#' @param fcol function for returning colours
+#' @param lv values for drawing in legend
+#' @param cachePath path to directory for storing cached files
+#' @param staticPath path for serving static assets from
+#' @param fplot function to plot a timeseries of data (see details)
+#' @param ferr function to process errors and return messages (see details)
+#' @param port port to serve data on
+#' @param clearCache logical, should cache be cleared if file changes
+#' 
+#' @details For a decription of fcol and lv see \code{scaleImage}. fplot shoudl take as its first input a POSIXct vector and as for its second a numeric vector of the same length and return a plot. ferror creates the response body contents from the standard R err object.
+#' @export
+serveTiles <- function(dataFile,fcol,lv=NULL,cachePath=NULL,staticPath=NULL,
+                       fplot=function(t,v){plot(t,v)},
+                       ferror=function(err){err$message},
+                       port=NULL,
+                       clearCache=FALSE){
+
+    ## set flags
+    flgs <- c(hasCache = !is.null(cachePath),
+              hasStatic = !is.null(staticPath),
+              hasLegend = !is.null(lv))
+    
+    ## open data file and take snapshot
+    dataFile <- normalizePath(dataFile)
+    rst <- terra::rast(normalizePath(dataFile))
+    snapshot <- fileSnapshot(dirname(dataFile))
+
+    ## deal with cache
+    if(flgs["cachePath"]){
+        cachePath <- normalizePath(cachePath)
+        if(!dir.exists(cachePath)){ dir.create(cachePath,recursive=TRUE) }
+    }
+    clearCache <- clearCache & flgs["cachePath"]
+    
+    ## #####################################################
+    ## helper functions
+    
+    isChanged <- function(){
+        tmp <- fileSnapshot(dirname(dataFile))
+        if( basename(dataFile) %in% changedFiles(snapshot,tmp)$changed ){
+            snapshot <<- tmp
+            rst <<- terra::rast(dataFile)
+            if(clearCache){
+                ##TODO check this removes files in folder but not folder
+                unlink(file.path(cachePath,"*"))
+            }
+            cat("reloading")
+        }
+        invisible(NULL)
+    }
+
+    ## #####################################################
+    ## route functions
+    
+    ## provide times
+    times_handler <- function(){
+        ## check for changes
+        isChanged()
+        sort( format(terra::time(rst),"%Y-%m-%dT%H:%M:%SZ",tz="GMT") )
+    }
+    
+    ## extract data as json - time series
+    json_handler <- function(lon,lat){
+        ## check if changed
+        isChanged()
+
+        pnt <- vect( matrix(c(lon,lat),1,2), crs="EPSG:3857" )
+        pnt <- terra::project(pnt,crs(rst))
+        
+        ts <- format(terra::time(rst),"%Y-%m-%dT%H:%M:%SZ",tz="GMT")
+        tv <- unlist( terra::extract(rst,pnt) )
+        idx <- !is.na(ts)
+        out <- data.frame(dateTime = unname(ts[idx]),
+                          value = unname(tv[idx])                      
+                          )
+        out[order(out$dateTime),]
+    }
+
+    
+    ## plot data
+    plot_handler <- function(lon,lat){
+        ## check if changed
+        isChanged()
+
+        pnt <- vect( matrix(c(lon,lat),1,2), crs="EPSG:3857" )
+        pnt <- terra::project(pnt,crs(rst))
+        
+        ts <- terra::time(rst)
+        tv <- unlist(terra::extract(rst,pnt))
+        idx <- !is.na(as.character(ts)) ## for some reason is.na returns FALSE for NA values!
+        fplot(ts[idx],tv[idx])
+    }
+
+    ## return tile as raw vector
+    tile_handler <- function(res,time,X,Y,Z){
+        ## check if changed
+        isChanged()
+
+        ## check time is valid
+        ts <- format(terra::time(rst),"%Y-%m-%dT%H:%M:%SZ",tz="UTC")
+        if(!(time %in% ts)){
+            stop( sprintf("Time stamp %s not valid or missing",time) )
+        }else{
+            ts <- which(ts==time)
+        }
+        
+        hasFile <- FALSE
+        if(hasCache){
+            fn <- file.path(cachePath,time,X,Y,paste0(Z,".png"))
+            hasFile <- file.exists(fn)
+        }
+        if(hasFile){
+            out <- readBin(fn,'raw',n = file.info(fn)$size)
+        }else{
+            out <- singleTime(rst[[ts]],fcol,Z,X,Y)
+            if(hasCache){
+                writeBin(out,fn)
+            }
+        }
+        out
+    }
+
+    ## legend handler
+    legend_handler <- function(res){
+        hasFile <- FALSE
+        if(hasCache){
+            fn <- file.path(cachePath,"legend.svg")
+            hasFile <- file.exists(fn)
+        }
+        if(hasFile){
+            out <- readLines(fn)
+        }else{
+            out <- scalePlot(fcol,lv)
+            if(hasCache){ writeLines(out,fn) }
+        }
+        out
+    }
+
+    ## error handler
+    error_handler <- function(req, res, err){
+        res$body <- ferror(err)
+        res$status <- 400
+        res
+    }
+
+    
+
+
+    ## ############################################################################
+    ## routes
+    ## ############################################################################
+    root <- pr()
+    root %>%
+        pr_filter("CORS", function(req,res){
+            res$setHeader("Access-Control-Allow-Origin", "*")
+            if (req$REQUEST_METHOD == "OPTIONS") {
+                res$setHeader("Access-Control-Allow-Methods","*")
+                res$setHeader("Access-Control-Allow-Headers", req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS)
+                res$status <- 200
+                return(list())
+            } else {
+                plumber::forward()
+            }
+        }) %>%
+        pr_get(path = "/times",
+               handler = times_handler,
+               serializer = serializer_unboxed_json(),
+               comments = "Times for which tiles are available"
+               ) %>%
+        pr_get(path = "/json/<lon:double>/<lat:double>",
+               handler = json_handler,
+               serializer = serializer_unboxed_json(),
+               comments = "Precipitation for the given location in mm"
+               ) %>%
+        pr_get(path = "/plot/<lon:double>/<lat:double>",
+               handler = plot_handler,
+               serializer = serializer_svg(width=10,height=8),
+               comments = "Plot of precipitation for the given location"
+               ) %>%
+        pr_get(path = "/tiles/<time>/<X:int>/<Y:int>/<Z:int>",
+               handler = tile_handler,
+               serializer = serializer_content_type("image/png"),
+               comments = "Returns tile as png image"
+               ) %>%
+        pr_static("/__docs__","./__docs__/") %>%
+        pr_set_error(error_handler)
+
+    if(flgs["hasLegend"]){
+        root %>%
+            pr_get(path = "/legend",
+                   handler = legend_handler,
+                   comments = "Returns legend as svg"
+                   )
+    }
+    if(flgs["hasStatic"]){
+        root %>% pr_static("/", normalizePath(staticPath) )
+    }
+
+    root %>% pr_run(port=port)
+}
